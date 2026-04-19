eb44306f2efc213855cb01faae71a06872ef6bc2
alumno <alumno@vm1>
2026-04-19 18:20:03 +0200

    Unidad 3 perfecta

--- app/Models/Matricula.php
+++ app/Models/Matricula.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Matricula extends Model
 {
-    //
+    public function modulo(): BelongsTo
+    {
+        return $this->belongsTo(Modulo::class);
+    }
+
+    public function estudiante(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'estudiante_id');
+    }
 }
